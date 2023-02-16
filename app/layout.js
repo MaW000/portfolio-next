202e1046381fc2b17f33d0802f82087cfc33090b
Matthew Wardlow <mwardlow0@gmail.com>
2023-02-16 17:09:23 -0500

    Cleaned up handling of imports

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body className="">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
