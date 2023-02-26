7a22b08ab26c61a84688446ee639f556cc495097
Matthew Wardlow <mwardlow0@gmail.com>
2023-02-26 18:32:16 -0500

    project section header more responsive

--- components/sections/ProjectSection.jsx
+++ components/sections/ProjectSection.jsx
@@ -13,13 +13,13 @@ const ProjectSection = () => {
       className="py-40  mx-auto"
     >
       <div className="flex items-center text-left justify-start mt-2 mb-10 text-lightOrange relative ">
-        <div className="3xl:ml-100 ml-97 flex mb-1 ">
+        <div className="absolute left-[25%] bottom-0 flex">
           <h1 className="mr-2.5 lg:text-lg 3xl:text-2xl">02.</h1>
           <h1 className="text-lg lg:text-xl 2xl:text-2xl leading-5 text-darkOrange   ">
             Some Things I`ve Built
           </h1>
         </div>
-        <div className="md:absolute bottom-0 right-[25%] h-[2px] w-3/6 bg-lightOrange  md:visible" />
+        <div className="md:absolute bottom-0 right-[25%] h-[2px] w-full md:w-3/6 bg-lightOrange  md:visible" />
       </div>
       <ul className="mx-5">
         <Projects />
