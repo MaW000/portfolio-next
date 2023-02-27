196b2c98520b2d0a94945015fcf90bfe5404d8c9
Matthew Wardlow <mwardlow0@gmail.com>
2023-02-27 18:37:31 -0500

    header buttons now drag to section

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -5,6 +5,37 @@ import { navVariants } from "@/motion";
 import { BsOctagon } from "react-icons/bs";
 import Image from "next/image";
 const Header = () => {
+  function scrollToTop(x) {
+    scrollTo(document.body, x, 1250);
+  }
+  function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+      change = to - start,
+      currentTime = 0,
+      increment = 20;
+
+    var animateScroll = function () {
+      currentTime += increment;
+      var val = Math.easeInOutQuad(currentTime, start, change, duration);
+      window.scrollBy({ top: val });
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+    animateScroll();
+  }
+
+  //t = current time
+  //b = start value
+  //c = change in value
+  //d = duration
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+  
   return (
     <motion.header
       variants={navVariants}
@@ -25,27 +56,27 @@ const Header = () => {
         </div>
       </a>
       <nav className="flex mx-auto flex-wrap items-center justify-center text-base lg:text-xl  3xl:text-2xl">
-        <a
-          href={"/"}
+        <button
+          onClick={() => scrollToTop(40)}
           className="headlink before:content-['|'] before:mr-1 after:ml-1 md:before:mr-5 md:after:ml-5  "
         >
           {" "}
           About
-        </a>
-        <a
-          href={"/"}
+        </button>
+        <button
+          onClick={() => scrollToTop(65)}
           className="headlink before:content-['|'] before:mr-1 after:ml-1  md:before:mr-5 md:after:ml-5  "
         >
           {" "}
           Work
-        </a>
-        <a
-          href={"/"}
+        </button>
+        <button
+          onClick={() => scrollToTop(110)}
           className="headlink before:content-['|'] before:mr-1 after:ml-1 md:before:mr-5 md:after:ml-5 after:content-['|']"
         >
           {" "}
           Contact
-        </a>
+        </button>
       </nav>
       <button className=" mx-auto font-mono px-4 py-2 my-auto text-xl 3xl:text-3xl  text-lightOrange bg-transparent rounded focus:outline-none hover:bg-darkOrange border-lightOrange border-2">
         Resume
