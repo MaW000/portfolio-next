8c17c58fb789221c3397e0106ebeb00abf4d2ae8
Matthew Wardlow <mwardlow0@gmail.com>
2023-02-28 04:00:55 -0500

    mobile change

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -3,7 +3,7 @@ import { Hero, AboutMe, CubeSpinner } from "../ui";
 
 const HeroSection = () => {
   return (
-    <section className="pt-32 mx-2 md:pt-44 lg:pt-60 2xl:pt-64 3xl:pt-96 min-h-screen">
+    <section className="pt-60 mx-2 md:pt-44 lg:pt-60 2xl:pt-64 3xl:pt-96 min-h-screen">
       <div className=" z-50 mx-5 md:mx-16 lg:mx-28 xl:mx-44 2xl:mx-64 2xl:mb-72 3xl:mx-96 3xl:mb-96">
         <Hero />
       </div>
