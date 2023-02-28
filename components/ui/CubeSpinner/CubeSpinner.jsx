e4cd411f3b22bf5aa2927d8b1b8b24ee7feafbc6
Matthew Wardlow <mwardlow0@gmail.com>
2023-02-28 03:57:03 -0500

    full w

--- components/ui/CubeSpinner/CubeSpinner.jsx
+++ components/ui/CubeSpinner/CubeSpinner.jsx
@@ -13,7 +13,7 @@ const CubeSpinner = () => {
         // variants={slideIn("right", "tween", 0.2, 1)}
         // initial="hidden"
         // whileInView="show"
-        className="absolute z-20 right-0 top-48 md:-top-[8rem] lg:-right-[30rem] lg:-top-[40rem] w-[320px] h-[844px] md:w-[600px] md:h-[1000px] lg:h-[2000px]   lg:w-[1200px] 2xl:w-[125%]"
+        className="absolute z-20 right-0 top-48 md:-top-[8rem] lg:-right-[30rem] lg:-top-[40rem] w-[125%] h-[844px] md:h-[1000px] lg:h-[2000px]   2xl:w-[125%]"
       >
         <Suspense fallback={null}>
           <Cube />
